b8e1cdcd53e94154f41854309b86a2f76d01e7c5
esparsh287 <esparsh.tamrakar18@gmail.com>
2024-10-03 17:45:53 +0545

    esp-contactUs

--- sugarcare/blogs/views.py
+++ sugarcare/blogs/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Post, Contact
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -16,19 +15,20 @@ def about(request):
     return render(request, 'blogs/about.html', {'title':"About Page"})
 
 def contact(request):
-    if request.method=='POST':
-        name=request.POST['name']
-        email=request.POST['email']
-        phone=request.POST['phone']
-        content=request.POST['content']
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        content = request.POST.get('content')
 
-        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
-            messages.error(request, "Please Fill the Form Correctly")
+        if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 4:
+            messages.error(request, "Please fill the form correctly")
         else:
-            contact= Contact(name=name, email=email, phone=phone, content=content)
+            contact = Contact(name=name, email=email, phone=phone, content=content)
             contact.save()
-            messages.success(request, "Your Message Has Been Received")
-    return render(request,'blogs/contact.html',{'title':"Contact Page"})
+            messages.success(request, "Your message has been received")
+
+    return render(request, 'blogs/contact.html', {'title': "Contact Page"})
 
 class PostListView(LoginRequiredMixin, ListView):
     model=Post
